96b4cf7f6812c08f4a69bae39dfc75459e494439
Goodminji <tksxk0@gmail.com>
2017-08-30 14:57:07 +0900

    익스플로우 버전 오류 수정

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@
 
 //상단 헤더 고정
 window.addEventListener('scroll', function(e) {
-	var top = (33+window.scrollY);
+	var top = (33+ (window.scrollY || document.documentElement.scrollTop));
 	document.getElementById("fixed_header").style.marginTop = top.toString()+"px";
 });
