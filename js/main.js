01fd62b2299be53fb1f6053aab91f7c230f7538a
Goodminji <tksxk0@gmail.com>
2017-08-30 11:11:08 +0900

    메인 상단 고정 수정 및 minjitest.html 삭제

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 /* main.js */
+
+//상단 헤더 고정
+window.addEventListener('scroll', function(e) {
+	var top = (33+window.scrollY);
+	document.getElementById("fixed_header").style.marginTop = top.toString()+"px";
+});
