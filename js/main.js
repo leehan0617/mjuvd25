69fd658cb9c80034faf0277345c781edcf986bf4
Goodminji <tksxk0@gmail.com>
2017-09-06 20:42:58 +0900

    20170906 전체 수정

--- js/main.js
+++
@@ -1,7 +0,0 @@
-/* main.js */
-
-//상단 헤더 고정
-window.addEventListener('scroll', function(e) {
-	var top = (33+ (window.scrollY || document.documentElement.scrollTop));
-	document.getElementById("fixed_header").style.marginTop = top.toString()+"px";
-});
