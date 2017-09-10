34021c7c8bf479178a8a1d757c59552994e23559
이한빈 <leehan0617@naver.com>
2017-09-11 00:23:42 +0900

    add subfiles

--- js/sub.js
+++ js/sub.js
@@ -1,5 +1,7 @@
 var projectFlag = true;
+var projectFlag2 = true;
 var memberFlag = true;
+var memberFlag2 = true;
 
 $(function() {
 	$('#project_arrow').on('click' , function() {
@@ -9,7 +11,16 @@ $(function() {
 			$('.project_category').show();
 		}
 		projectFlag = !projectFlag;
-	})
+	});
+
+	$('#project_arrow2').on('click' , function() {
+		if(projectFlag2) {
+			$('.project_category2').hide();
+		} else {
+			$('.project_category2').show();
+		}
+		projectFlag2 = !projectFlag2;
+	});
 
 	$('#member_arrow').on('click' , function() {
 		if(memberFlag) {
@@ -19,6 +30,16 @@ $(function() {
 		}
 
 		memberFlag = !memberFlag;
+	});
+
+	$('#member_arrow2').on('click' , function() {
+		if(memberFlag2) {
+			$('.member_div2').hide();
+		} else {
+			$('.member_div2').show();
+		}
+
+		memberFlag2 = !memberFlag2;
 	});
 
 	$('.vimeo_link').on('click' , function() {
